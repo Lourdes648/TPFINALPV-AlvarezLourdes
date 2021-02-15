72a29735aa47df1d1ab3b6d086afca124efc2933
alumno <alumno@EM43269479>
2021-02-15 08:54:41 -0300

    Inicializacion del proyecto

---
+++ src/main/java/ar/edu/unju/edm/ProyectoFinalPv1Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalPv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalPv1Application.class, args);
+	}
+
+}
